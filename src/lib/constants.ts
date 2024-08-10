8cbcfd8fa09a9ce3edfeacfed029046523d12412
MDHASIBASKARI <mdhsbaskari@gmail.com>
2024-08-11 01:15:08 +0600

    refactor: Update TOKEN_KEY constant to use 'access_token' instead of 'token'

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -2,4 +2,4 @@
 // export const WS_URL: string = 'ws://localhost:5000';
 export const BASE_URL: string = 'https://page-connect-hwdbfkgkdygjeca7.eastus-01.azurewebsites.net';
 export const WS_URL: string = 'https://page-connect-hwdbfkgkdygjeca7.eastus-01.azurewebsites.net';
-export const TOKEN_KEY: string = 'token';
+export const TOKEN_KEY: string = 'access_token';
